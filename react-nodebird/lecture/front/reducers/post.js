32b7b413de20634e9eb82f928008a297814dc838
heroyooi <heroyooi1018@gmail.com>
2021-03-30 23:11:03 +0900

    집에서 노드버드 공부중

---
+++ react-nodebird/lecture/front/reducers/post.js
@@ -0,0 +1,69 @@
+export const initialState = {
+  mainPosts: [
+    {
+      id: 1,
+      User: {
+        id: 1,
+        nickname: '제로초',
+      },
+      content: '첫 번째 게시글 #해시태그 #익스프레스',
+      Images: [
+        {
+          src: 'http://heroyooi.ivyro.net/origin/img/portfolio/autobell.jpg',
+        },
+        {
+          src: 'http://heroyooi.ivyro.net/origin/img/portfolio/toonnbook.png',
+        },
+        {
+          src: 'http://heroyooi.ivyro.net/origin/img/portfolio/ocb2.5.png',
+        },
+      ],
+      Comments: [
+        {
+          User: {
+            nickname: 'nero',
+          },
+          content: '우와 개정판이 나왔군요~',
+        },
+        {
+          User: {
+            nickname: 'hero',
+          },
+          content: '얼른 사고싶어요~',
+        },
+      ],
+    },
+  ],
+  imagePaths: [],
+  postAdded: false,
+};
+
+const ADD_POST = 'ADD_POST';
+export const addPost = {
+  type: ADD_POST,
+};
+const dummyPost = {
+  id: 2,
+  content: '더미데이터입니다.',
+  User: {
+    id: 1,
+    nickname: '제로초',
+  },
+  Images: [],
+  Comments: [],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        mainPosts: [dummyPost, ...state.mainPosts],
+        postAdded: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
