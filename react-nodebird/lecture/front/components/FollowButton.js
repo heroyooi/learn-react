3159907ed7ecac762e0638c277d28a1bd7107d16
heroyooi <heroyooi1018@gmail.com>
2021-04-05 15:54:28 +0900

    4강까지 공부 끝

--- react-nodebird/lecture/front/components/FollowButton.js
+++ react-nodebird/lecture/front/components/FollowButton.js
@@ -1,17 +1,32 @@
 import React, { useCallback } from 'react';
 import { Button } from 'antd';
 import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { UNFOLLOW_REQUEST, FOLLOW_REQUEST } from '../reducers/user';
 
 const FollowButton = ({ post }) => {
-  const { me } = useSelector((state) => state.user);
-  const onFollow = useCallback(() => {}, []);
+  const dispatch = useDispatch();
+  const { me, followLoading, unfollowLoading } = useSelector((state) => state.user);
   const isFollowing = me?.Followings.find((v) => v.id === post.User.id);
-  return <Button>팔로우</Button>;
+  const onClickButton = useCallback(() => {
+    if (isFollowing) {
+      dispatch({
+        type: UNFOLLOW_REQUEST,
+        data: post.User.id,
+      });
+    } else {
+      dispatch({
+        type: FOLLOW_REQUEST,
+        data: post.User.id,
+      });
+    }
+  }, [isFollowing]);
+
+  return <Button loading={followLoading || unfollowLoading} onClick={onClickButton}>{isFollowing ? '언팔로우' : '팔로우'}</Button>;
 };
 
 FollowButton.propTypes = {
-  post: PropTypes.object.isReuqired,
+  post: PropTypes.object.isRequired,
 };
 
 export default FollowButton;
--- react-nodebird/lecture/front/sagas/user.js
+++ react-nodebird/lecture/front/sagas/user.js
@@ -9,11 +9,17 @@ import {
   SIGN_UP_REQUEST,
   SIGN_UP_SUCCESS,
   SIGN_UP_FAILURE,
+  FOLLOW_REQUEST,
+  FOLLOW_SUCCESS,
+  FOLLOW_FAILURE,
+  UNFOLLOW_REQUEST,
+  UNFOLLOW_SUCCESS,
+  UNFOLLOW_FAILURE,
 } from '../reducers/user';
 
-// function logInAPI(data) {
-//   return axios.post('/api/login', data);
-// }
+function logInAPI(data) {
+  return axios.post('/api/login', data);
+}
 
 function* logIn(action) {
   try {
@@ -33,9 +39,9 @@ function* logIn(action) {
   }
 }
 
-// function logOutAPI() {
-//   return axios.post('/api/logout');
-// }
+function logOutAPI() {
+  return axios.post('/api/logout');
+}
 
 function* logOut() {
   try {
@@ -53,9 +59,9 @@ function* logOut() {
   }
 }
 
-// function signUpAPI() {
-//   return axios.post('/api/signup');
-// }
+function signUpAPI() {
+  return axios.post('/api/signup');
+}
 
 function* signUp() {
   try {
@@ -73,6 +79,56 @@ function* signUp() {
   }
 }
 
+function followAPI() {
+  return axios.post('/api/follow');
+}
+
+function* follow(action) {
+  try {
+    // const result = yield call(followAPI);
+    yield delay(1000);
+    yield put({
+      type: FOLLOW_SUCCESS,
+      data: action.data,
+    });
+  } catch (err) {
+    console.error(err);
+    yield put({
+      type: FOLLOW_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+function unfollowAPI() {
+  return axios.post('/api/unfollow');
+}
+
+function* unfollow(action) {
+  try {
+    // const result = yield call(followAPI);
+    yield delay(1000);
+    yield put({
+      type: UNFOLLOW_SUCCESS,
+      data: action.data,
+    });
+  } catch (err) {
+    console.error(err);
+    yield put({
+      type: UNFOLLOW_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+function* watchFollow() {
+  yield takeLatest(FOLLOW_REQUEST, follow);
+}
+
+function* watchUnfollow() {
+  yield takeLatest(UNFOLLOW_REQUEST, unfollow);
+}
+
 function* watchLogIn() {
   yield takeLatest(LOG_IN_REQUEST, logIn);
 }
@@ -86,5 +142,5 @@ function* watchSignUp() {
 }
 
 export default function* userSaga() {
-  yield all([fork(watchLogIn), fork(watchLogOut), fork(watchSignUp)]);
+  yield all([fork(watchFollow), fork(watchUnfollow), fork(watchLogIn), fork(watchLogOut), fork(watchSignUp)]);
 }
