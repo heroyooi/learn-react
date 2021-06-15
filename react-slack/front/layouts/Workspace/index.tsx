86a8f18f58c1a4844e4ded5f1b66e712523f36d4
heroyooi <heroyooi1018@gmail.com>
2021-06-16 00:33:04 +0900

    aaa

--- react-slack/front/layouts/Workspace/index.tsx
+++ react-slack/front/layouts/Workspace/index.tsx
@@ -119,7 +119,9 @@ const Workspace: VFC = () => {
     setShowCreateChannelModal(true);
   }, []);
 
-  const onClickInviteWorkspace = useCallback(() => {}, []);
+  const onClickInviteWorkspace = useCallback(() => {
+    setShowInviteWorkspaceModal(true);
+  }, []);
 
   if (!userData) {
     return <Redirect to="/login" />;
--- react-slack/front/pages/DirectMessage/index.tsx
+++ react-slack/front/pages/DirectMessage/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import gravatar from 'gravatar';
+import { Container, Header } from './styles';
 
 const DirectMessage = () => {
-  return <div>DM</div>;
+  return (
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+        <span>{userData.nickname}</span>
+      </Header>
+    </Container>
+  );
 };
 
 export default DirectMessage;
